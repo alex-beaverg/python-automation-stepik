5c1f78d0efd41b3317ae8a96e887f8f74ad4b7de
Alexey Bobrikov <a.beaverg@gmail.com>
2023-05-24 02:24:05 +0300

    update files

--- section3/lesson2_step13.py
+++ section3/lesson2_step13.py
@@ -1,8 +1,8 @@
 """
-Run with unittest:
-    python lesson2_step13.py
 Run with PyTest:
-    pytest -v lesson2_step13.py
+    pytest -s -v lesson2_step13.py
+    pytest -s -v --browser_name=firefox lesson2_step13.py
+    pytest -s -v --browser_name=chrome lesson2_step13.py
 """
 
 from selenium import webdriver
@@ -10,48 +10,46 @@ from selenium.webdriver.common.by import By
 import unittest
 
 
-class TestClass (unittest.TestCase):
-    def test_registration1(self):
-        try:
-            link = "http://suninjuly.github.io/registration1.html"
-            browser = webdriver.Chrome()
-            browser.maximize_window()
-            browser.get(link)
+def test_registration1(browser):
+    try:
+        link = "http://suninjuly.github.io/registration1.html"
+        browser.maximize_window()
+        browser.get(link)
 
-            browser.find_element(By.CSS_SELECTOR, ".first_block .first").send_keys("Alex")
-            browser.find_element(By.CSS_SELECTOR, ".first_block .second").send_keys("Popovko")
-            browser.find_element(By.CSS_SELECTOR, ".first_block .third").send_keys("a.popovko@mail.app")
-            browser.find_element(By.CSS_SELECTOR, ".second_block .first").send_keys("+111(22)333-44-55")
-            browser.find_element(By.CSS_SELECTOR, ".second_block .second").send_keys("address")
-            browser.find_element(By.CSS_SELECTOR, "button.btn").click()
+        browser.find_element(By.CSS_SELECTOR, ".first_block .first").send_keys("Alex")
+        browser.find_element(By.CSS_SELECTOR, ".first_block .second").send_keys("Popovko")
+        browser.find_element(By.CSS_SELECTOR, ".first_block .third").send_keys("a.popovko@mail.app")
+        browser.find_element(By.CSS_SELECTOR, ".second_block .first").send_keys("+111(22)333-44-55")
+        browser.find_element(By.CSS_SELECTOR, ".second_block .second").send_keys("address")
+        browser.find_element(By.CSS_SELECTOR, "button.btn").click()
 
-            welcome_text = browser.find_element(By.TAG_NAME, "h1").text
+        welcome_text = browser.find_element(By.TAG_NAME, "h1").text
 
-            self.assertEqual(welcome_text, "Congratulations! You have successfully registered!",
-                             "Expected text on Registration page 1 was not found")
-        finally:
-            browser.quit()
+        assert welcome_text == "Congratulations! You have successfully registered!", \
+            "Expected text on Registration page 1 was not found"
+    finally:
+        browser.quit()
 
-    def test_registration2(self):
-        try:
-            link = "http://suninjuly.github.io/registration2.html"
-            browser = webdriver.Chrome()
-            browser.maximize_window()
-            browser.get(link)
 
-            browser.find_element(By.CSS_SELECTOR, ".first_block .first").send_keys("Alex")
-            browser.find_element(By.CSS_SELECTOR, ".first_block .second").send_keys("Popovko")
-            browser.find_element(By.CSS_SELECTOR, ".first_block .third").send_keys("a.popovko@mail.app")
-            browser.find_element(By.CSS_SELECTOR, ".second_block .first").send_keys("+111(22)333-44-55")
-            browser.find_element(By.CSS_SELECTOR, ".second_block .second").send_keys("address")
-            browser.find_element(By.CSS_SELECTOR, "button.btn").click()
+def test_registration2(browser):
+    try:
+        link = "http://suninjuly.github.io/registration2.html"
+        browser.maximize_window()
+        browser.get(link)
 
-            welcome_text = browser.find_element(By.TAG_NAME, "h1").text
+        browser.find_element(By.CSS_SELECTOR, ".first_block .first").send_keys("Alex")
+        browser.find_element(By.CSS_SELECTOR, ".first_block .second").send_keys("Popovko")
+        browser.find_element(By.CSS_SELECTOR, ".first_block .third").send_keys("a.popovko@mail.app")
+        browser.find_element(By.CSS_SELECTOR, ".second_block .first").send_keys("+111(22)333-44-55")
+        browser.find_element(By.CSS_SELECTOR, ".second_block .second").send_keys("address")
+        browser.find_element(By.CSS_SELECTOR, "button.btn").click()
 
-            self.assertEqual(welcome_text, "Congratulations! You have successfully registered!",
-                             "Expected text on Registration page 2 was not found")
-        finally:
-            browser.quit()
+        welcome_text = browser.find_element(By.TAG_NAME, "h1").text
+
+        assert welcome_text == "Congratulations! You have successfully registered!", \
+            "Expected text on Registration page 2 was not found"
+    finally:
+        browser.quit()
 
 
 if __name__ == '__main__':
